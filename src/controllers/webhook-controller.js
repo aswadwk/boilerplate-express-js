c339b26b844a031713858c61866dbda7799e9453
aswadwk <hajaraswadkom@gmail.com>
2025-04-26 11:51:33 +0700

    refactor: simplify response structure in handleWebHookWhatsapp function

--- src/controllers/webhook-controller.js
+++ src/controllers/webhook-controller.js
@@ -2,13 +2,7 @@ import whatsappService from '../services/whatsapp-service.js';
 
 const handleWebHookWhatsapp = async (req, res, next) => {
     try {
-        const result = await whatsappService.handleWebHook(req);
-
-        res.status(200).json({
-            status: true,
-            message: 'Webhook received successfully.',
-            data: result,
-        });
+        res.status(200).send(await whatsappService.handleWebHook(req));
     } catch (error) {
         next(error);
     }
--- src/services/whatsapp-service.js
+++ src/services/whatsapp-service.js
@@ -1,23 +1,18 @@
 import ResponseError from '../commons/response-error.js';
 
 const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN || 'secret';
-// const MY_TOKEN = process.env.WHATSAPP_MY_TOKEN || '';
 
 const handleWebHook = async (request) => {
-    console.log('Webhook received Query:', request.query);
-
     const mode = request.query['hub.mode'];
     const challange = request.query['hub.challenge'];
     const token = request.query['hub.verify_token'];
 
     if (mode && token) {
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
             return challange;
         }
     }
 
-    console.log('Invalid token');
     throw new ResponseError(403, 'Unauthorized');
 };
 
